147e7a3946fd9fd34141915ceda31f3bbf0f6ca9
piti118@gmail.com <piti118@gmail.com>
2016-05-19 21:06:55 +0700

    stage 3

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 
 var app = express()
+
+app.use(bodyParser.json())
 
 app.get('/', function(req, res){
   res.json('Hello world')
@@ -11,9 +14,22 @@ var data = {
   PJ: {hp:98, mp:99, strength: 0, special: "low-priority"},
 }
 
-app.get('/tamagotchi/:name', function(req,res){
+app.get('/tamagotchi/:name', function(req, res){
   var name = req.params.name
-  console.log(name)
+  if (! (name in data) ){
+    res.sendStatus(400)
+  }
+  console.log('get info', name)
+  res.json(data[name])
+})
+
+app.post('/tamagotchi/hit/:name', function(req, res){
+  var damage = req.json.damage
+  var name = req.params.name
+  if (! (name in data) ){
+    res.sendStatus(400)
+  }
+  data[name].hp = data[name].hp - damage
   res.json(data[name])
 })
 
