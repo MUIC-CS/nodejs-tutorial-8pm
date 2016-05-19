d5cbddb6f6c4456d0e09c141460e77afce42d27b
piti118@gmail.com <piti118@gmail.com>
2016-05-19 20:50:13 +0700

    stage 2

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ app.get('/', function(req, res){
   res.json('Hello world')
 })
 
+var data = {
+  ham: {hp:100, mp:50, strength: 30, special: "ham.net"},
+  PJ: {hp:98, mp:99, strength: 0, special: "low-priority"},
+}
+
+app.get('/tamagotchi/:name', function(req,res){
+  var name = req.params.name
+  console.log(name)
+  res.json(data[name])
+})
+
 app.listen(3000)
